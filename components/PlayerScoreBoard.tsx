b95465110cc27836fe22def0a48a380e3d97d208
Nisit Jiraruwat <nisid.ji@backyard.in.th>
2024-05-08 12:23:08 +0700

    Fix mobile size

--- components/PlayerScoreBoard.tsx
+++ components/PlayerScoreBoard.tsx
@@ -20,34 +20,36 @@ export default function PlayerScoreBoard () {
   return (
     <div className='grid grid-cols-2 gap-y-4 rounded-md bg-[#e2be8b] p-2 laptop:grid-cols-1 laptop:py-4'>
       <div>
-        <div className='whitespace-nowrap text-center text-xl font-bold text-[#a32335]'>Click</div>
+        <div className='whitespace-nowrap text-center text-lg font-bold text-[#a32335] laptop:text-xl'>Click</div>
         <div
           data-testid="click-count"
           className='text-center text-lg'
         >{clickCount}</div>
       </div>
       <div>
-        <div className='whitespace-nowrap text-center text-xl font-bold text-pink-900'>My Best Score</div>
+        <div className='whitespace-nowrap text-center text-lg font-bold text-pink-900 laptop:text-xl'>My Best Score</div>
         <div
           data-testid="my-best-score"
           className='text-center text-lg'
         >{displayMyBestScore}</div>
       </div>
       <div>
-        <div className='whitespace-nowrap text-center text-xl font-bold text-yellow-900'>Global Best Score</div>
+        <div className='whitespace-nowrap text-center text-lg font-bold text-yellow-900 laptop:text-xl'>Global Best Score</div>
         <div
           data-testid="global-best-score"
           className='text-center text-lg'
         >{displayGlobalBestScore}</div>
       </div>
-      <div className='flex justify-end laptop:justify-center'>
-        <button
-          data-testid="new-game-btn"
-          className='rounded bg-slate-700 px-4 py-2 text-xl font-bold text-orange-500 hover:bg-slate-900'
-          onClick={newGame}
-        >
-          NEW GAME
-        </button>
+      <div className='flex items-end justify-end laptop:justify-center'>
+        <div>
+          <button
+            data-testid="new-game-btn"
+            className='rounded bg-slate-700 px-4 py-2 text-lg font-bold text-orange-500 hover:bg-slate-900 laptop:text-xl'
+            onClick={newGame}
+          >
+            NEW GAME
+          </button>
+        </div>
       </div>
     </div>
   )
